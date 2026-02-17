b43175fb5de13e790a72e50c0587dcdbedf1bcb1
Joanna J Rios <joannarios526@gmail.com>
2026-02-17 14:12:36 -0600

    refactored App routing to support conditional page layouts. note: i removed the global header/footer to let individual pages manage their own layouts. due to this there was also some content i removed that valeria had added to try and force the footer to not cover the entry pages content.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,28 @@
-import { BrowserRouter as Router, Routes, Route, useLocation} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 // importing pages
 import Home from "./pages/Home"; // home page
 import SignUp from "./pages/SignUp"; // sign up page
 import Login from "./pages/Login"; // login page
+import Dashboard from "./pages/Dashboard.tsx"; // dashboard page for students to view their activity and reservations
 
-
-// import components
+/* comenting out the header and footer imports here because we are now rendering them inside the pages instead of in App, this is because we want to conditionally render different headers and footers for different pages (i.e. no header or footer for home/login/signup, student header for dashboard, romms,equipment, etc.) */
+/* //import components
 import Header from "./components/Header.tsx"; // header component for landing page navbar
 import Footer from "./components/Footer.tsx"; // footer component for footer content
+*/
 
-// placeholder for future routes
-const Dashboard = () => <div>Student Dashboard Page</div>;
+/* commenting this out for now as i made changes to the header and footer to render inside the pages instead of in App, this is because we want to conditionally render different headers and footers for different pages (i.e. no header or footer for home/login/signup, student header for dashboard, romms,equipment, etc. */
+/*
+const NavigationWrapper = () => {
+  const location = useLocation();
+  // Show StudentHeader for dashboard and related student pages (i.e. all pages aften login), otherwise show Header
+  // if path is "private" (dashboard, rooms, equipment) show StudentHeader, if path is "public" (home, login, signup) show Header
+  const isStudentPage = location.pathname.startsWith('/dashboard') || location.pathname.startsWith('/rooms') || location.pathname.startsWith('/equipment');
+  return isStudentPage ? <StudentHeader /> : <Header />;
+};
+
+
 
 // created a wrapper component to access useLocation (this can't be usde inside App)
 function AppWrapper() {
@@ -26,19 +37,28 @@ function AppWrapper() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} /> {/* Placeholder route for student dashboard */}
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </main>
       {!hideFooter && <Footer />}
     </>
   );
 }
+*/
 
 function App() {
   return (
     <Router>
-      <AppWrapper />
-    </Router>
-  )
+      <main className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </main>
+      </Router>
+  );
 }
+
 export default App;
